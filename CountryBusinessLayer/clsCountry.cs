604f4041653fbd265d2a5618c32e3dd6a056d842
DalalYouness <younessdalal1@gmail.com>
2025-02-23 13:50:50 +0100

    preparing Business and DataAccess Layers for Countries

---
+++ CountryBusinessLayer/clsCountry.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Data;
+using CountryDataAccessLayer;
+
+namespace CountryBusinessLayer
+{
+    public class clsCountry
+    {
+    }
+}
---
+++ CountryPresentationLayer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data;
+using CountryBusinessLayer;
+
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+
